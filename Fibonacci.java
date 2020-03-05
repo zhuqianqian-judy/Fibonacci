a399995417ce4cab0006f61b8f1a0c3a906e433b
zhuqianqian-judy <40655640+zhuqianqian-judy@users.noreply.github.com>
2020-03-05 23:52:45 +0800

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,11 +6,12 @@ public class Fibonacci{
 		else{
 			return of(i-1) + of(i-2);
 		}
-  }
+	}
 
 	public static void main(String[] args){
 	
-    for(int i=1; i <= 200; i++){
+		for(int i=1; i <= 200; i++){
+			
 			System.out.println(Fibonacci.of(i));
 		
 		}
