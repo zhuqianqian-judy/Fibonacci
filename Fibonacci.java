5852f1570247c5552e9aa5ba193a1188c294ca92
zhuqianqian-judy <40655640+zhuqianqian-judy@users.noreply.github.com>
2020-03-05 23:51:01 +0800

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci{
+	public static int of(int i){
+		if(i == 1 || i == 2){
+			return 1;
+		}
+		else{
+			return of(i-1) + of(i-2);
+		}
+  }
+
+	public static void main(String[] args){
+	
+    for(int i=1; i <= 200; i++){
+			System.out.println(Fibonacci.of(i));
+		
+		}
+	}	
+}
